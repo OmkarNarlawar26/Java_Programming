5f83ad8f2497a657e56d0c7b908c35c4e4869b1e
Omkar Narlawar <omkarnarlawar26@gmail.com>
2025-10-07 09:43:36 +0530

    Update FinalClass2.java

--- FinalClass2.java
+++ FinalClass2.java
@@ -1,20 +1,28 @@
 
 final class Base
 {
-   
-}
+    public void fun()
+    {
+        System.out.println("Inside Base fun");
+    }
+}`
 
-class Derived extends Base
+class Derived
 {
-    
+    public Base bobj;       // Composition
+
+    public Derived()
+    {
+        bobj = new Base();
+    }
 }
 
 class FinalClass2
 {
     public static void main(String A[])
     {
+        Derived dobj = new Derived();
 
-
+        dobj.bobj.fun();
     }
-
 }
